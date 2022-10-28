0d82fabc4341a02b51c75412688715da3c6c1082
hasansheikh1 <hasanfaysal17@gmail.com>
2022-10-28 20:50:24 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdX9BkXBW8TcWkcPgDH00RBoaunevOI6U",
+  authDomain: "fir-crud-react-8108d.firebaseapp.com",
+  projectId: "fir-crud-react-8108d",
+  storageBucket: "fir-crud-react-8108d.appspot.com",
+  messagingSenderId: "141604460143",
+  appId: "1:141604460143:web:4363ab9504e43cea519f9a",
+  measurementId: "G-XNZTEMQ823",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
 
 function App() {
+  // const [data, setData] = useState([]);
+  const [postText, setPostText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    //     const getData=async()=>{
+    //     const querySnapshot = await getDocs(collection(db, "posts"));
+    //     querySnapshot.forEach((doc) => {
+    //     console.log(`${doc.id} =>`, doc.data());
+
+    //      setPosts((prev)=>{
+
+    //       let newArr=[...prev,doc.data()];
+    //       return newArr;
+    //      })
+
+    // });
+    // }
+    //   getData();
+
+    const getRealtimeData = async () => {
+
+      const q = query(collection(db, "posts"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const cities = [];
+        querySnapshot.forEach((doc) => {
+          cities.push(doc.data().name);
+        });
+        console.log("Posts: ", posts);
+      });
+
+      const unsub = onSnapshot(doc(db, "posts", "SF"), (doc) => {
+        console.log("Current data: ", doc.data());
+      });
+      
+    }
+
+    getRealtimeData();
+
+  }, []);
+
+  const savePost = async (e) => {
+    // axios.get("");
+    e.preventDefault();
+    console.log("Post text", postText);
+    try {
+      const docRef = await addDoc(collection(db, "posts"), {
+        text: postText,
+        createdOn: new Date().getTime(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={savePost}>
+        {/* <input
+          type="text"
+          id="cityName"
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          placeholder="Enter City.."
+        /> */}
+
+        <div className="wrap">
+          <div className="search">
+            <input
+              id="cityName"
+              onChange={(e) => {
+                setPostText(e.target.value);
+              }}
+              type="text"
+              className="searchTerm"
+              placeholder="What are you looking for?"
+            />
+            <button type="submit" className="searchButton">
+              Search!
+            </button>
+          </div>
+        </div>
+
+        {/* <div> */}
+
+        {posts.map((eachPost, i) => (
+          <div className="post" key={i}>
+            <h1>{eachPost?.text}</h1>
+
+            <span>
+              {moment(eachPost?.datePublished).format("MMMM Do YYYY, h:mm a")}
+            </span>
+
+            <button className="btn-read">
+              <a className="btn-read1" target="_blank" href={eachPost.url}>
+                Read More
+              </a>
+            </button>
+          </div>
+        ))}
+
+        {/* </div> */}
+      </form>
     </div>
   );
 }
